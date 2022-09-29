1b797a9ba5c5fc24c5498800ae73f8b0d0903bbe
Alysa Vega <92405765+alyliann@users.noreply.github.com>
2022-09-29 15:21:22 -0400

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,19 @@
+//Author: Alysa Liann Vega
+//Course: CSCI-135
+//Instructor: Tong Yi
+//Date: February 2, 2022
+
+//This program takes the user-inputted age and prints it
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int age = 0;
+	cout << "Enter your age: " << endl;
+	cin >> age;
+
+	cout << "Your age is " << age << "." << endl;
+}
